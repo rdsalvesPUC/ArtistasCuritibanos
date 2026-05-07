8c6afc3a25cf3b866da55626eac8fa07e2f22a91
rodrigo.salves <s.rodrigo5@pucpr.edu.br>
2026-05-07 18:48:10 -0300

    Initial Commit

---
+++ ArtistasCuritibanos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArtistasCuritibanos
+//
+//  Created by RODRIGO DA SILVA ALVES on 07/05/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
